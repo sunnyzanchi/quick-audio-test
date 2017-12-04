8b1a8ee62fb9b574419e703913570479b3c5c204
Eric Zanchi <ezanchi@gsandf.com>
2017-12-04 16:37:12 -0600

    Fixed typo, added bell sound file

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import { Howl } from 'howler';
 const sound = new Howl({
   src: ['bell.wav']
 });
+
 const howlerButton = document.querySelector('#howler button');
 
-howerButton.addEventListener('click', function(e) {
+howlerButton.addEventListener('click', function(e) {
   sound.play();
 });
